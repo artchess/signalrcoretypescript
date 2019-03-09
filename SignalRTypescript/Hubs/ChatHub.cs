76f7971d953cc67f90f223ee05c05fb0b523bdb0
Arturo Ferreiro Personal <a.ferreirov@hotmail.com>
2019-03-09 16:38:28 -0600

    Agrego codigo para el hub y para el cliente.

--- SignalRTypescript/Hubs/ChatHub.cs
+++ SignalRTypescript/Hubs/ChatHub.cs
@@ -8,5 +8,9 @@ namespace SignalRTypescript.Hubs
 {
     public class ChatHub: Hub
     {
+        public async Task NewMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("messageReceived", username, message);
+        }
     }
 }
